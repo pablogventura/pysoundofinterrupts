afb871bbb7ea6c4bef32f28d0f22eecbd89248c7
tade <tadeojcocucci@gmail.com>
2021-10-08 09:23:54 -0300

    liveplot de interrupts separados por tipo

---
+++ interruptsplot.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+import sys
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+interr_types = [
+    'LOC',      # Local timer interrupts
+    'RES',      # Rescheduling interrupts
+    'CAL',      # Function call interrupts
+    'TLB'       # TLB shootdowns
+]
+
+colors = [
+    'tab:red',
+    'tab:blue',
+    'tab:green',
+    'tab:orange'
+]
+
+x_max = 100
+y_max = 5000
+
+def accumulated_interrupts():
+    """
+    Parser de /proc/interrupts para ver las interrupciones acumuladas
+    """
+    result = {}
+    with open("/proc/interrupts", "r") as f:
+        cpus = len(next(f).split())  # cantidad de cpus
+        for line in f:
+            line = line.split()
+            interrupt_t = line[0].strip(":")
+            if interrupt_t in interr_types:
+                try:
+                    result[interrupt_t] = sum(
+                            [float(i) for i in line[1:cpus+1]])
+                except:
+                    # linea para descartar
+                    continue
+    return result
+
+
+def intpersec():
+    """
+    interrupciones por segundo, como es un generador hay que usarlo asi:
+    it = intspersec()   # para inicializar
+    next(it)            # para obtener el siguiente valor
+    """
+    last_interrupts = accumulated_interrupts()
+    last_time = time.time()
+    while True:
+        current_interrupts = accumulated_interrupts()  # medicion actual
+        current_time = time.time()
+        period = current_time - last_time
+        last_time = current_time
+        result = { k: (current_interrupts[k] - last_interrupts[k]) / period
+            for k in interr_types
+        }
+        last_interrupts = current_interrupts.copy()
+        yield result
+
+def init_plot():
+    for line in lines:
+        line.set_data([], [])
+    return lines
+
+
+def animate(i):
+    interrupts = next(it)
+    for j, line in enumerate(lines):
+        y = list(line.get_ydata())
+        if len(y) >= x_max:
+            y.pop(0)
+        y.append(interrupts[interr_types[j]])
+        line.set_data(range(len(y)), y)
+    return lines
+
+
+it = intpersec()
+
+fig = plt.figure()
+ax = plt.axes(xlim=(0, x_max), ylim=(0, y_max))
+lines = []
+for j in range(len(interr_types)):
+    line = ax.plot([], [],
+            ls='-', marker='.', color=colors[j], label=interr_types[j])[0]
+    lines.append(line)
+
+anim = animation.FuncAnimation(fig, animate,
+        frames=None, interval=50, blit=False, init_func=init_plot)
+fig.legend()
+plt.show()
